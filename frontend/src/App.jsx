76d766c9762a14b9ee437740fc996a9ac293a32b
You Song Hou <ysongweb3@gmail.com>
2023-10-08 22:02:15 -0400

    feat: install and add Chakra UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,24 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
+import { ChakraProvider, Text } from '@chakra-ui/react';
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route
-          path="/test"
-          element={
-            <>
-              <h1>Test</h1>
-            </>} />
-        <Route
-          path="/"
-          element={
-            <h1>Home</h1>} />
-      </Routes>
-    </HashRouter>
+    <ChakraProvider>
+      <HashRouter>
+        <Routes>
+          <Route
+            path="/test"
+            element={
+              <>
+                <h1>Test</h1>
+              </>} />
+          <Route
+            path="/"
+            element={
+              <h1>Home</h1>} />
+        </Routes>
+      </HashRouter>
+    </ChakraProvider>
   )
 }
 
