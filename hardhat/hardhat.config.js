b466359928e70cbe65bf8028ed04da54e863e6ee
You Song Hou <ysongweb3@gmail.com>
2023-10-18 01:44:46 -0400

    feat: update hardhat.config.ts to deploy a contract on Scroll Sepolia Testnet

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -4,6 +4,14 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    // npx hardhat run --network scrollSepolia scripts/deploy.js
+    scrollSepolia: {
+      url: "https://sepolia-rpc.scroll.io/" || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
   paths: {
     artifacts: '../frontend/src/artifacts',
     cache: '../frontend/src/cache',
