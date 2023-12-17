3014bdd81631a3ad3853e2d37033fadf88e9d86f
You Song Hou <ysongweb3@gmail.com>
2023-12-16 23:09:31 -0500

    feat: install @ethereum-attestation-service/eas-sdk

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Link as ReactLink } from 'react-router-dom';
 import { Container, Box, Flex, Heading, Spacer, Badge, Button, Link } from '@chakra-ui/react';
+import { EAS, Offchain, SchemaEncoder, SchemaRegistry } from "@ethereum-attestation-service/eas-sdk";
 import { ethers } from 'ethers';
 
 import ProjectOriginalityCheck from "../../artifacts/contracts/ProjectOriginalityCheck.sol/ProjectOriginalityCheck.json";
@@ -9,26 +10,37 @@ import ProjectNFT from "../../artifacts/contracts/ProjectNFT.sol/ProjectNFT.json
 const LOCAL_PROJECT_CONTRACT_ADDRESS = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
 const LOCAL_PROJECT_NFT_CONTRACT_ADDRESS = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707";
 
+export const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia v0.26
+
 function Navbar({ ethAddress, setETHAddress, setProjectContract, setnftContract }) {
   const [networkName, setnetworkName] = useState("");
 
   const connectMetamask = async () => {
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
     setETHAddress(accounts[0]);
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    //const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    // Initialize the sdk with the address of the EAS Schema contract address
+    const eas = new EAS(EASContractAddress);
+
+    // Gets a default provider (in production use something else like infura/alchemy)
+    const provider = ethers.providers.getDefaultProvider(
+      "sepolia"
+    );
+
+    // Connects an ethers style provider/signingProvider to perform read/write functions.
+    // MUST be a signer to do write operations!
+    eas.connect(provider);
 
     const { name, chainId } = await provider.getNetwork();
     console.log(name, chainId);
     setnetworkName(name);
 
-    const signer = provider.getSigner();
-    console.log(signer);
+    // const contract1 = new ethers.Contract(LOCAL_PROJECT_CONTRACT_ADDRESS, ProjectOriginalityCheck.abi, signer);
+    // setProjectContract(contract1);
 
-    const contract1 = new ethers.Contract(LOCAL_PROJECT_CONTRACT_ADDRESS, ProjectOriginalityCheck.abi, signer);
-    setProjectContract(contract1);
-
-    const contract2 = new ethers.Contract(LOCAL_PROJECT_NFT_CONTRACT_ADDRESS, ProjectNFT.abi, signer);
-    setnftContract(contract2)
+    // const contract2 = new ethers.Contract(LOCAL_PROJECT_NFT_CONTRACT_ADDRESS, ProjectNFT.abi, signer);
+    // setnftContract(contract2)
   }
 
   return (
