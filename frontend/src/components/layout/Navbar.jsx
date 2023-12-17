f12b6209f80d2454062f1ad78430607cef663777
You Song Hou <ysongweb3@gmail.com>
2023-12-17 00:10:03 -0500

    feat: implement a function to retrieve an on-chain attestation for a given UID

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -36,11 +36,21 @@ function Navbar({ ethAddress, setETHAddress, setProjectContract, setnftContract 
     console.log(name, chainId);
     setnetworkName(name);
 
+    getAttestation(eas);
+
     // const contract1 = new ethers.Contract(LOCAL_PROJECT_CONTRACT_ADDRESS, ProjectOriginalityCheck.abi, signer);
     // setProjectContract(contract1);
 
     // const contract2 = new ethers.Contract(LOCAL_PROJECT_NFT_CONTRACT_ADDRESS, ProjectNFT.abi, signer);
     // setnftContract(contract2)
+  }
+
+  const getAttestation = async (eas) => {
+    const uid = "0xff08bbf3d3e6e0992fc70ab9b9370416be59e87897c3d42b20549901d2cccc3e";
+
+    const attestation = await eas.getAttestation(uid);
+
+    console.log(attestation);
   }
 
   return (
