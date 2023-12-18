4ed65c709e4adbd7e2546438fcf1bdac71826b9a
You Song Hou <ysongweb3@gmail.com>
2023-12-18 00:43:11 -0500

    feat: implement a function to create an off-chain attestation

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -39,7 +39,8 @@ function Navbar({ ethAddress, setETHAddress, setProjectContract, setnftContract 
     setnetworkName(name);
 
     getAttestation(eas);
-    createOnchainAttestations(eas);
+    // createOnchainAttestations(eas);
+    createOffchainAttestations(eas, signer);
 
     // const contract1 = new ethers.Contract(LOCAL_PROJECT_CONTRACT_ADDRESS, ProjectOriginalityCheck.abi, signer);
     // setProjectContract(contract1);
@@ -78,6 +79,32 @@ function Navbar({ ethAddress, setETHAddress, setProjectContract, setnftContract 
     const newAttestationUID = await tx.wait();
     
     console.log("New attestation UID:", newAttestationUID);
+  }
+
+  const createOffchainAttestations = async (eas, signer) => {
+    const offchain = await eas.getOffchain();
+
+    // Initialize SchemaEncoder with the schema string
+    const schemaEncoder = new SchemaEncoder("uint256 eventId, uint8 voteIndex");
+    const encodedData = schemaEncoder.encodeData([
+      { name: "eventId", value: 1, type: "uint256" },
+      { name: "voteIndex", value: 1, type: "uint8" },
+    ]);
+    
+    const offchainAttestation = await offchain.signOffchainAttestation({
+      recipient: '0xaa90e02e88047232288D01Fe04d846e8A4Cc88dd',
+      // Unix timestamp of when attestation expires. (0 for no expiration)
+      expirationTime: 0,
+      // Unix timestamp of current time
+      time: 1671219636,
+      revocable: true, // Be aware that if your schema is not revocable, this MUST be false
+      version: 1,
+      nonce: 0,
+      schema: "0xb16fa048b0d597f5a821747eba64efa4762ee5143e9a80600d0005386edfc995",
+      refUID: '0x0000000000000000000000000000000000000000000000000000000000000000',
+      data: encodedData,
+    }, signer);
+    console.log(offchainAttestation);
   }
 
   return (
